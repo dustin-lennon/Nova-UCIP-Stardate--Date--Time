15024615d50cf078197e26a6f29a86564a4176b1
Dustin <demonicpagan@gmail.com>
2010-11-09 17:03:36 -0600

    Working on fixing how date/time is displayed in case server is in an area that observes DST.
    Signed-off-by: Dustin <demonicpagan@gmail.com>

--- application/hooks/UCIP-Stardate-Hook.php
+++ application/hooks/UCIP-Stardate-Hook.php
@@ -22,9 +22,11 @@ class Mod {
 		$ci =& get_instance();
 		
 		$output = '<div style="padding:1em;">';
-		$output.= '<strong>Stardate:</strong> '. $ci->settings->get_setting('sim_year') . date('m.d') .'<br />';
-		$output.= '<strong>Date:</strong> '. date('M d, Y') .'<br />';
-		$output.= '<strong>Time:</strong> '. date('g:i A T');
+		$output .= '<strong>Stardate:</strong> '. $ci->settings->get_setting('sim_year') . date('m.d') .'<br />';
+		$output .= '<strong>Date:</strong> '. date('M d, Y') .'<br />';
+		$output .= '<strong>Time:</strong> '. date('g:i A T', gmt_to_local(time(), $ci->settings->get_setting('timezone'), $ci->settings->get_setting('daylight_savings'))).'<br />';
+		$output .= '<strong>Temp:</strong> '.date('g:i A T', now()).'<br />';
+		$output .= '<strong>Temp2:</strong> '.gmt_to_local(now(), $ci->settings->get_setting('timezone'), $ci->settings->get_setting('daylight_savings'));
 		$output.= '</div>';
 		
 		$ci->template->add_region('stardate');
